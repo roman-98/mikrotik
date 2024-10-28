859796055bc9cd35d39ccb0e55eda8102c9725d8
Roman Strypa <romanstripa@gmail.com>
2024-10-28 13:01:22 +0000

    selected-logs&logID

--- scripts/LogInfoToTelegram.sh
+++ scripts/LogInfoToTelegram.sh
@@ -10,26 +10,23 @@
 :local logLineCount [ :len $logEntries ];
 
 if ($logLineCount > 0) do={
-    :local currentTime "$[ /log get [ :pick $logEntries ($logLineCount - 1) ] time ]";
-    :if ([:len $currentTime] = 10) do={
-        :set currentTime [ :pick $currentTime 0 10 ];
-    }
-    
-    :local logMessage $[/log get [ :pick $logEntries ($logLineCount - 1) ] message];
-    :local logType $[/log get [ :pick $logEntries ($logLineCount - 1) ] topics];
-
-    :if (($logType = "critical") || 
-        ($logType = "l2tp") ||
-        ($logType = "ipsec") || 
-        ($logType = "system") || 
-        ($logType = "info") || 
-        ($logType = "account") || 
-        ($logType = "dhcp")) do={
-
-        :set output "$currentTime - $logMessage";
+    :foreach logId in=$logEntries do={
+        :local logMessage $[/log get $logId message];
+        :local logTime $[/log get $logId time];
+        :local logType $[/log get $logId topics]; 
         
-        :if (([:len $lastTime] < 1) || (([:len $lastTime] > 0) && ($lastTime != $currentTime))) do={
-            :set lastTime $currentTime;
+        :if (($lastLogId < $logId) && 
+            (($logType = "critical") || 
+             ($logType = "l2tp") ||
+             ($logType = "ipsec") ||  
+             ($logType = "system") || 
+             ($logType = "info") || 
+             ($logType = "account") || 
+             ($logType = "dhcp"))) do={
+             
+            :set output "$logTime - $logMessage";
+            :set lastLogId $logId;
+            
             :tool fetch url=("https://api.telegram.org/bot$TelegramApi/sendMessage?chat_id=$ChatId&text=%F0%9F%93%A1\"$mtIdentity\" :  $output") keep-result=no;
         }
     }
